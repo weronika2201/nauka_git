979b511b50ce1ee923e3ea6f6972ba2f578724a0
weronika2201 <weronika2201@users.noreply.github.com>
2020-12-13 10:22:36 +0100

    drugie rozwiazanie

--- zadanie_2.py
+++ zadanie_2.py
@@ -9,3 +9,12 @@ def calculate_vat(netto):
 if __name__ == "__main__":
     vat = calculate_vat(1000)
     print("{0}".format(vat))
+
+
+# drugi sposob
+def calucate_vat(netto):
+    return (netto * 23)/100
+
+if __name__ == "__main__":
+    vat = calucate_vat(777)
+    print("{​​​​0}​​​​".format(vat))
