c6c3bd671d344a7bf55b31b42ce2da72920755ce
weronika2201 <weronika2201.users.noreplay.github.com>
2020-12-13 14:00:33 +0100

    test kalkulator

--- test_calculator.py
+++ test_calculator.py
@@ -6,3 +6,19 @@ class TestCalculator(unittest.TestCase):
     def test_dodwanie(self):
         r = calculate(1, 2, '+')
         self.assertEqual(r, 3)
+
+    def test_odejmowanie(self):
+        r = calculate(5, 3, '-')
+        self.assertEqual(r, 2)
+
+    def test_mnozenie(self):
+        r = calculate(2, 2, '*')
+        self.assertEqual(r, 4)
+
+    def test_dzielenie(self):
+        r = calculate(20, 2, '/')
+        self.assertEqual(r, 10)
+
+    def test_reszta(self):
+        r = calculate(4, 3, '/')
+        self.assertEqual(r, 1)
