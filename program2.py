dc6a77b9f343eba505321987a0e1f8c3cf938d81
weronika2201 <weronika2201@users.noreply.github.com>
2020-12-13 10:06:02 +0100

    drugie rozwiazanie

--- program2.py
+++ program2.py
@@ -13,3 +13,21 @@ if __name__ == "__main__":
     samolot['typ'] = samolot['type']
     del samolot['type']
     print_dict(samolot)
+
+# drugi sposob
+
+def print_dict(d):
+    for key in samolot:
+        print("{​​​​0}​​​​:{​​​​1}​​​​".format(key, d[key]))
+
+if __name__ == "__main__":
+    samolot = {​​​​'name': 'boeing',
+           'przebieg': 10000,
+           'type': 'pasazerski'}​​​​
+
+    samolot['nazwa'] = samolot['name']
+    samolot.pop('name')
+    samolot['typ'] = samolot['type']
+    samolot.pop('type')
+
+    print_dict(samolot)
