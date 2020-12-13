93dbbdbaef1de968d8be3e2c44f68e0c7a8381c7
weronika2201 <weronika2201@users.noreply.github.com>
2020-12-13 10:57:31 +0100

    zadanie 3

--- zadanie_3.py
+++ zadanie_3.py
@@ -1,5 +1,16 @@
 
-koszyk = {'produkt': 'mleko', 'ilosc': 1, 'cena': 1.5,
-'produkt2': 'czekolada', 'ilosc2': 2, 'cena2': 3}
+koszyk = [
+    {'produkt': 'mleko', 'ilosc': 1, 'cena': 1.5},
+    {'produkt': 'czekolada', 'ilosc': 2, 'cena': 3},
+    {'produkt': 'konserwa', 'ilosc': 4, 'cena': 3.5}
+]
 
-print(koszyk)
+suma = 0
+for poz in koszyk:
+    il = poz['ilosc']
+    c = poz ['cena']
+    suma = suma + (c * il)
+    #print(c)
+    #print(suma)
+    #print(poz)
+print(suma)
